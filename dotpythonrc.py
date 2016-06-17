6e23140e57c41bc419d140036ae1f59f8ddb9b5e
Bogdan Hodorog <bogdan.hodorog@domain.com.au>
2016-06-17 16:11:13 +1000

    Latest changes from work.

--- dotpythonrc.py
+++ dotpythonrc.py
@@ -1,24 +1,16 @@
-from atexit import register
-from os import path
-import sys
-import readline
-import rlcompleter
-
-# Sets up a tab for completion (use a single space to indent Python code).
-readline.parse_and_bind('tab: complete')
-
-historyPath = path.expanduser('~/.python_history')
-readline.set_history_length(1000)
-
-# Reads the history of the previous session, if it exists.
-if path.exists(historyPath):
-    readline.read_history_file(historyPath)
-
-# Sets up history saving on exit.
-def save_history(historyPath=historyPath, readline=readline):
-    readline.write_history_file(historyPath)
-
-register(save_history)
-
-# Cleans up the global name
-del register, path, readline, rlcompleter, historyPath, save_history
+try:
+    import readline
+    import rlcompleter
+    import atexit
+    import os
+except ImportError:
+    print "Python shell enhancement modules not available."
+else:
+    histfile = os.path.join(os.environ["HOME"], ".pythonhistory")
+    import rlcompleter
+    readline.parse_and_bind("tab: complete")
+    if os.path.isfile(histfile):
+        readline.read_history_file(histfile)
+    atexit.register(readline.write_history_file, histfile)
+    del os, histfile, readline, rlcompleter, atexit
+    print "Python shell history and tab completion are enabled."
